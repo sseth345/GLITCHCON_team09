0d6c4fc0998ad0e78df69bdabd88da75806c41f1
Shreyash Gautam <155219117+shreyashgautam@users.noreply.github.com>
2026-03-10 12:24:38 +0530

    Update app.js

--- backend1/app.js
+++ backend1/app.js
@@ -10,7 +10,11 @@ function createApp(io) {
   const app = express();
 
   app.use(helmet({ contentSecurityPolicy: false }));
-  app.use(cors());
+ app.use(cors({
+  origin: "*",
+  methods: ["GET","POST","PUT","DELETE"],
+  credentials: true
+}));
   app.options('*', cors());
   app.use(express.json({ limit: '10mb' }));
   app.use(express.urlencoded({ extended: true }));
